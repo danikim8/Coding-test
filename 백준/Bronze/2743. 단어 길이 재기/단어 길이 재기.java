dd7dad9dacd9af5e39ef1eb42b15f3eb88783bcb
Danhee Kim <127651317+danikim8@users.noreply.github.com>
2023-11-19 18:56:34 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 196 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //5단계 - 2
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(s.length());
+    }
+}
