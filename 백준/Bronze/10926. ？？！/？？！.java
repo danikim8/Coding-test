ba89393725887766a531713fdc76e89254721c0b
Danhee Kim <127651317+danikim8@users.noreply.github.com>
2023-11-19 18:49:15 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //1단계 - 7
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        System.out.println(s1+"??!");
+    }
+}
