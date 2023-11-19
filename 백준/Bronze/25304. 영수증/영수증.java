b9383f072ec8ba758cc320c61dceb94b031beff0
Danhee Kim <127651317+danikim8@users.noreply.github.com>
2023-11-19 18:52:43 +0900

    [Bronze IV] Title: 영수증, Time: 220 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //3단계 - 4
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        int n = sc.nextInt();
+        int sum =0;
+        for(int i=0; i<n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            sum += a*b;
+        }
+        if (sum == total){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
