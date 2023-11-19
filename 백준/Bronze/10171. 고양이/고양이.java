310e45bda4fcb9212d4cdc9123355df4d3363489
Danhee Kim <127651317+danikim8@users.noreply.github.com>
2023-11-19 18:50:13 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 14036 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //1단계 - 12
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
