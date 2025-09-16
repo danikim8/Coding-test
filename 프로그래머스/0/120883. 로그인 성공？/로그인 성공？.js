5f1d3b52f19a090ed541744de108d22d01fa4d7c
Dani Kim <127651317+danikim8@users.noreply.github.com>
2025-09-16 16:51:40 +0900

    [level 0] Title: 로그인 성공?, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,14 @@
+function solution(id_pw, db) {
+    const [inputId, inputPw] = id_pw;
+    
+    for (let [id, pw] of db) {
+        if (id === inputId) {
+            if (pw === inputPw){
+                return "login";
+            } else {
+                return "wrong pw";
+            }
+        }
+    }
+    return "fail";
+}
