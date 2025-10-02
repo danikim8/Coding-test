8e800e838b662d6388dfa6bafe2d22da839e0ef2
Dani Kim <127651317+danikim8@users.noreply.github.com>
2025-10-02 20:03:58 +0900

    [level 1] Title: 명예의 전당 (1), Time: 21.22 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,22 +1,15 @@
 function solution(k, score) {
-    const hallOfFame = [];
-    const result = [];
-
-    for (let i = 0; i < score.length; i++) {
-        // 새로운 점수 추가
+    const hallOfFame = []
+    const result = []
+    
+    for(let i=0; i<score.length; i++){
         hallOfFame.push(score[i]);
-
-        // 내림차순 정렬
-        hallOfFame.sort((a, b) => b - a);
-
-        // 상위 k명만 유지
-        if (hallOfFame.length > k) {
+        hallOfFame.sort((a,b) => b-a);
+        
+        if(hallOfFame.length > k){
             hallOfFame.pop();
         }
-
-        // 현재 명예의 전당 최하위 점수 = 마지막 원소
         result.push(hallOfFame[hallOfFame.length - 1]);
     }
-
     return result;
 }
